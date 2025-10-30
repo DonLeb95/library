d96431d9a1081626503839e5ab11eef1d546da36
Donnie <82501498+DonLeb95@users.noreply.github.com>
2025-10-30 00:58:09 -0400

    Make function to create and add  user inputs to library array

--- files/script.js
+++ files/script.js
@@ -5,9 +5,18 @@ function Book(title,author,pubDate,pages){
     this.pages = pages;
 }
 
-function addBook (title,author,pubDate,pages){
+function addBook (title,author,pubDate,pages,readState){
     const book = new Book(title,author,pubDate,pages);
-    book.id = crypto.randomUUID();
+    book.id = crypto.randomUUID(); 
+    const readAns = (a) => {
+    if(a == "true"){
+        return "yes"
+    }
+    else if(a == "false"){
+        return "no"
+    } else return "unknown"
+    }
+    book.read = readAns(readState);
     library.push(book);
 }
 
@@ -33,3 +42,19 @@ newBookButton.addEventListener("click",(e) => {
     document.querySelector("dialog").showModal();
 });
 
+const bookFormButtons = document.querySelector(`#submit, #cancel`);
+bookFormButtons.addEventListener("click",(e) => {
+    e.preventDefault();
+    const bookForm = new FormData(document.querySelector("#new_book_entry"));
+    const userTitle = bookForm.get("title");
+    const userAuthor = bookForm.get("author");
+    const userPubDate = bookForm.get("pubDate");
+    const userPages = bookForm.get("pages");
+    const userReadState = bookForm.get("read");
+
+    if(e.target.textContent == "Add Book"){
+        addBook(userTitle,userAuthor,userPubDate,userPages,userReadState);
+        document.querySelector("dialog").close();
+    } else 
+    if(e.target.textContent == "Cancel"){document.querySelector("dialog").close()}
+});
